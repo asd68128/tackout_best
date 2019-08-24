a5b769ba549c959e6eff07ff57ef1afc9dcd01b0
asd68128 <435675756@qq.com>
2019-08-24 10:51:55 +0800

    dd

--- src/main/java/com/sl/tackout/pojo/BusinessTable.java
+++ src/main/java/com/sl/tackout/pojo/BusinessTable.java
@@ -15,7 +15,7 @@ public class BusinessTable {
     private int businessState;//商家状态（0表示商家已注销，1表示正常营业，2表示商家已休息）
     private String businessInfo;//商家信息（简介）
     private String businessImg;//商家图片
-    private String shopName;//商家店名
+    private String shopName;//商家店名bbbbbbbbbbbb
 
     public BusinessTable() {
     }
